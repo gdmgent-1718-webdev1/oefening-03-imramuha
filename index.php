f69f3bb0180534400177ca93cef74e2c78ddbfd4
imranmuha <imramuha@student.arteveldehogeschool.be>
2017-10-05 15:51:29 +0200

    Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+     
+
+        <?php $x = 10?>
+        <?php $y = 7?>
+
+        <h5><?php echo $x + $y?></h5>
+        <h5><?php echo $x - $y?></h5>
+        <h5><?php echo $x * $y?></h5>
+        <h5><?php echo $x % $y?></h5>
+    
+</body>
+</html>
